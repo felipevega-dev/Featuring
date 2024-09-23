6356418a012ad3c9f1f44e44b0563a9f3a5c0f49
felipevega-dev <felipevega.dev@gmail.com>
2024-09-22 22:53:04 -0300

    un detalle

--- Featuring-app/app/index.tsx
+++ Featuring-app/app/index.tsx
@@ -1,6 +1,6 @@
 import { Redirect } from "expo-router";
 
 const Home = () => {
-  return <Redirect href="/(auth)\welcome"></Redirect>;
+  return <Redirect href="/(auth)/welcome"></Redirect>;
 };
 export default Home;
